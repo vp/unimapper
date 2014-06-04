e15fa08ea4fce8b5d32ca0e04a89b373c8ea977e
bauer01 <bronislav.sedlak@gmail.com>
2014-06-04 19:07:44 +0200

    : support for associations

--- src/query/FindOne.php
+++ src/query/FindOne.php
@@ -5,7 +5,6 @@ namespace UniMapper\Query;
 use UniMapper\Query\IConditionable,
     UniMapper\Exceptions\PropertyTypeException,
     UniMapper\Exceptions\QueryException,
-    UniMapper\Mapper,
     UniMapper\Reflection;
 
 class FindOne extends \UniMapper\Query implements IConditionable
@@ -14,9 +13,19 @@ class FindOne extends \UniMapper\Query implements IConditionable
     /** @var mixed */
     public $primaryValue;
 
-    public function __construct(Reflection\Entity $entityReflection, Mapper $mapper, $primaryValue)
+    /** @var array */
+    private $associations = [
+        "local" => [],
+        "remote" => []
+    ];
+
+    public function __construct(Reflection\Entity $entityReflection, array $mappers, $primaryValue)
     {
-        parent::__construct($entityReflection, $mapper);
+        parent::__construct($entityReflection, $mappers);
+
+        if (!$entityReflection->hasPrimaryProperty()) {
+            throw new QueryException("Can not use findOne() on entity without primary property!");
+        }
 
         try {
             $entityReflection->getPrimaryProperty()->validateValue($primaryValue);
@@ -27,12 +36,34 @@ class FindOne extends \UniMapper\Query implements IConditionable
         $this->primaryValue = $primaryValue;
     }
 
+    public function associate($propertyName)
+    {
+        if (!isset($this->entityReflection->getProperties()[$propertyName])) {
+            throw new QueryException("Property '" . $propertyName . "' not defined!");
+        }
+
+        $property = $this->entityReflection->getProperties()[$propertyName];
+        if (!$property->isAssociation()) {
+            throw new QueryException("Property '" . $propertyName . "' is not defined as association!");
+        }
+
+        $association = $property->getAssociation();
+        if ($association->isRemote()) {
+            $this->associations["remote"][$property->getName()] = $association;
+        } else {
+            $this->associations["local"][$property->getName()] = $association;
+        }
+
+        return $this;
+    }
+
     public function onExecute(\UniMapper\Mapper $mapper)
     {
         $result = $mapper->findOne(
             $this->entityReflection->getMapperReflection()->getResource(),
             $this->entityReflection->getPrimaryProperty()->getMappedName(),
-            $this->primaryValue
+            $this->primaryValue,
+            $this->associations["local"]
         );
 
         if (!$result) {
